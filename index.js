7fae0f059ff67f86a31516891de02bcabb71f151
Arthur Bernier <ma2bernier@gmail.com>
2021-04-02 19:01:41 -0400

    Minor Changes

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const QUESTIONS = [
     {
       name: 'project-choice',
       type: 'list',
-      message: 'If You Ready To Make Your Project witcha boi Big Poppa Code Select Get Started or else press CTRL + C to stop',
+      message: 'If You Ready To Make Your Project witcha boi Big Poppa Code Select Which Project You Want To Build or else press CTRL + C to stop',
       choices: CHOICES
     },
     {
@@ -44,28 +44,31 @@ const QUESTIONS = [
       consola.info('Coding is hard most people give up before they get this far...')
       consola.info('But your here now coding, using React an advanced UI Library');
       consola.info('Whoever you are just know, if no one else has told you...')
-      consola.info('Ya boi Big Poppa Code is proud of you.... \n o yea and this could take a while')
-      exec(`cd ${projectName} && npm install`, function (err, stdout, stderr ){
+      consola.info('Ya boi Big Poppa Code is proud of you.... \n o yea and this could take a while sit patient I am installing your dependencies')
+      exec(`cd ${projectName} && yarn install`, function (err, stdout, stderr ){
         if (err) {
           consola.error(`Dude heres your error it no big deal keep pushing: ${err}`)
           consola.warn(`These are warnings but they could help you also : ${stderr}`)
           return;
         }else {
-          consola.warn(stdout);
+          consola.success(stdout);
           exec(`cd ${projectName} && npm audit`, function (err, stdout, stderr ){
-            consola.warn('If you have any vulnerabilities trust that \n BrowserSync,Webpack, Gulp, React, and Babel are probably fixing them \n As long as its a dev dependency it wont effect our production code or the lesson, ask me if your not sure')
-            consola.warn(stdout)
-            consola.success('Looks like we built your React Project inside the directory you suggested, you can scroll up and see your output and any NPM Warnings :) ')
+            consola.info('I just checked for vulnerabilities \n If you have any vulnerabilities trust that \n BrowserSync,Webpack, Gulp, React, and Babel are probably fixing them \n As long as its a dev dependency it wont effect our production code or the lesson, ask me if your not sure')
+            consola.success(stdout)
+            consola.success('Looks like everything is GUCCI we built your React Project inside the directory you suggested, you can scroll up and see your output and any NPM Warnings :) ')
             consola.success(`It is finished.... well not really you still need to cd into the ${projectName} folder by running \n
               --------->   cd ${projectName}    <---------`)
+
             consola.success(`To help here is some useful info: \n
-              you need to make sure you have gulp installed \n
-              -----> npm i -g gulp-cli <----- \n
-              to start the app you can run \n
-              ----> npm run dev <----- \n
-              If your Hungry For More \n you can hack the project yourself by editing  \n
-              ------------> .babelrc (controls babel), gulpfile.js (controls gulp), webpack.config.js (controls webpack) <--------------
+              to start the app you can run one of the following commands...
               `)
+              consola.success(`For The React Starter ----> yarn start <-----`)
+              consola.success(`For The Final Starter ----> yarn dev <----- \n`)
+              consola.success(`If your Hungry For More \n you can hack the final project starter yourself by editing  \n
+              .babelrc (controls babel), <--------------  \n
+              gulpfile.js (controls gulp), <-------------- \n
+              webpack.config.js (controls webpack) <-------------- \n`)
+              consola.success(`  Project Built, Node Modules added etc, thanks to the power of Recursion(Recusrion(Recursion(...))) \n`)
               consola.success(`happy coding ....... `)
 
           })
