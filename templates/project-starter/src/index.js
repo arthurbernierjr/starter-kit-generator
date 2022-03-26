2b4c77362820e268dd6b845e95a0914807f7410d
Arthur Bernier Jr <ma2bernier@gmail.com>
2022-03-25 21:20:21 -0400

    Minor Changes

--- templates/project-starter/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AppRouter from './router';
-import 'bootstrap/scss/bootstrap.scss';
-
-const app = document.getElementById('app');
-
-ReactDOM.render(<AppRouter />, app);
