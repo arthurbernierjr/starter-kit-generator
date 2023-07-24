ab2f48d19728a12ebf2e9be0edadc2d9581379cf
Arthur Bernier Jr <ma2bernier@gmail.com>
2023-07-23 23:19:01 -0700

    Minor Changes
    New update

--- templates/getting-started-with-mern/src/pages/Home.js
+++ templates/getting-started-with-mern/src/pages/Home.js
@@ -1,5 +1,12 @@
 import { useState } from 'react';
 
 export default function Home(props) {
-	return <div className="HomePage">This is the {props.page} page</div>;
+	return(
+		<div className="HomePage">
+			This is the {props.page} page
+			<center>
+				<img src="https://media.git.generalassemb.ly/user/15881/files/8b5cd280-1f37-11ea-9c27-c9bb294aa61f"/>
+			</center>
+		</div>
+	) 
 }
